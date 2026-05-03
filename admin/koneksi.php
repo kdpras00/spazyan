346ff027c56c4c0b5f449cec5ee8040f5837b2e3
kdpras00 <kdpras00@gmail.com>
2026-05-04 04:49:22 +0700

    chore: update database connection credentials and error handling in koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$koneksi = mysqli_connect ("127.0.0.1","root","","spazyan");
+$koneksi = mysqli_connect("localhost", "kure8737_spazyan", "Panggung00@", "kure8737_spazyan");
 
-if(mysqli_connect_error()){
-    echo "Koneksi Database Gagal : .mysqli_connect_error()";
+if (!$koneksi) {
+    die("Koneksi Database Gagal: " . mysqli_connect_error());
 }
 
 // Cek timeout session secara global
@@ -12,5 +12,4 @@ $auth_path = $is_admin_dir ? '../auth_timeout.php' : 'auth_timeout.php';
 if (file_exists($auth_path)) {
     include $auth_path;
 }
-
 ?>
